890ecb6464a551f302cd92a535d98c43976ddab2
anitamtsai <166353693+anitamtsai@users.noreply.github.com>
2026-03-29 18:17:20 -0400

    Add files via upload

---
+++ garrett.js
@@ -0,0 +1,20 @@
+const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
+let pics = document.querySelectorAll(".spacer img")
+
+for (let i = 0; i < pics.length; i++) {
+    console.log("Looking at " + pics[i].src);
+
+    pics[i].addEventListener("error", function (){
+
+    if (reduceMotion) {
+    this.src= "https://github.com/anitamtsai/garrettfinal/blob/main/images/runnerpng.png?raw=true";
+}
+    else{
+    this.src='https://raw.githubusercontent.com/anitamtsai/garrettfinal/refs/heads/main/images/runnergif.gif';
+    this.alt = "image is not available";
+}
+    })
+} 
+
+/*grabbed code from discussion & attended Sami's office hours*/
