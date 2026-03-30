b8a12b7be7682a7f5a375e65cf7f1071bd7c65a3
anitamtsai <166353693+anitamtsai@users.noreply.github.com>
2026-03-29 21:18:06 -0400

    edit
    Updated image source paths and cleaned up code formatting.

--- js/garrett.js
+++ js/garrett.js
@@ -1,20 +1,38 @@
+// const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+
+// let pics = document.querySelectorAll(".spacer img")
+
+// for (let i = 0; i < pics.length; i++) {
+//     console.log("Looking at " + pics[i].src);
+
+//     pics[i].addEventListener("error", function (){
+
+//     if (reduceMotion) {
+//     this.src= 'https://raw.githubusercontent.com/anitamtsai/garrettfinal/main/images/runnerpng.png';
+// }
+//     else{
+//     this.src='https://raw.githubusercontent.com/anitamtsai/garrettfinal/main/images/runnergif.gif';
+//     this.alt = "image is not available";
+// }
+//     })
+// } 
+
+// /*grabbed code from discussion & attended Sami's office hours*/
+
+
 const reduceMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
 
-let pics = document.querySelectorAll(".spacer img")
+let pics = document.querySelectorAll(".spacer img");
 
 for (let i = 0; i < pics.length; i++) {
-    console.log("Looking at " + pics[i].src);
+  console.log("Looking at " + pics[i].src);
 
-    pics[i].addEventListener("error", function (){
-
+  pics[i].addEventListener("error", function () {
     if (reduceMotion) {
-    this.src= 'https://raw.githubusercontent.com/anitamtsai/garrettfinal/main/images/runnerpng.png';
-}
-    else{
-    this.src='https://raw.githubusercontent.com/anitamtsai/garrettfinal/main/images/runnergif.gif';
+      this.src = "images/runnerpng.png";
+    } else {
+      this.src = "images/runnergif.gif";
+    }
     this.alt = "image is not available";
+  });
 }
-    })
-} 
-
-/*grabbed code from discussion & attended Sami's office hours*/
